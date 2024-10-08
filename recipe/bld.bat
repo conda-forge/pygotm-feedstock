19e15f57fbc11cf0dc77aaec3d3f5475c25cb3e7
Jorn Bruggeman <jorn@bolding-bruggeman.com>
2024-10-08 10:05:19 +0100

    move to latest gfortran and ninja on windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,12 @@
+REM From https://github.com/conda-forge/netcdf-fortran-feedstock/blob/main/recipe/bld.bat
+set "HOST=x86_64-w64-mingw32"
+set "CC=%HOST%-gcc.exe"
+set "FC=%HOST%-gfortran.exe"
 
 (
 echo [build_ext]
-echo cmake_opts=-DPython3_EXECUTABLE="%PYTHON%" -DCMAKE_C_COMPILER=gcc -G "MinGW Makefiles"
-echo compiler=gfortran
+echo cmake_opts=-DPython3_EXECUTABLE="%PYTHON%" -DCMAKE_C_COMPILER=%CC% -G "Ninja"
+echo compiler=%FC%
 ) > "%SRC_DIR%\setup.cfg"
 
 set CFLAGS=-DMS_WIN64
